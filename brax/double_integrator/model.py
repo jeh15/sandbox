TODO: Debug train.py -- JIT time too long. QP layer is recieving incorrect dims only for train.py works for model_test.py...

brax/double_integrator/model.py
@@ -48,9 +48,9 @@ class ActorCriticNetwork(nn.Module):
             name='layer_7',
             dtype=dtype,
         )
-        self.dense_8 = nn.Dense(
-            features=features,
-            name='layer_8',
+        self.target_prediction = nn.Dense(
+            features=1,
+            name='target_prediction_layer',
             dtype=dtype,
         )
         self.mean_layer = nn.Dense(
@@ -86,29 +86,30 @@ class ActorCriticNetwork(nn.Module):
         self.osqp_layer = jax.vmap(
             qp_func,
             in_axes=(0, 0),
-            out_axes=(0, 0),
+            out_axes=(0, 0, 0),
         )
 
+    # Embedded MPC Actor-Critic Network:
     def model(self, x):
         range = 2.0
+        # Initial Conditions:
+        initial_conditions = x
         # Shared Layers:
         x = self.dense_1(x)
         x = nn.tanh(x)
         x = self.dense_2(x)
         x = nn.tanh(x)
         # Policy Layer:
-        y = self.dense_3(x)
+        target_position = self.target_prediction(x)
+        pos, vel, acc = self.osqp_layer(initial_conditions, target_position)
+        y = self.dense_4(acc)
         y = nn.tanh(y)
-        y = self.dense_4(y)
-        y = nn.tanh(y)
-        z = self.dense_5(x)
-        z = nn.tanh(z)
-        z = self.dense_6(z)
+        z = self.dense_5(acc)
         z = nn.tanh(z)
         # Value Layer
-        w = self.dense_7(x)
+        w = self.dense_6(x)
         w = nn.tanh(w)
-        w = self.dense_8(w)
+        w = self.dense_7(w)
         w = nn.tanh(w)
         # Output Layer:
         mean = self.mean_layer(y)

brax/double_integrator/puck.py
@@ -9,7 +9,7 @@ import jax
 import jax.numpy as jnp
 
 
-class CartPole(env.PipelineEnv):
+class Puck(env.PipelineEnv):
 
     def __init__(self, backend='generalized', **kwargs):
         filename = r'puck.xml'
@@ -68,11 +68,11 @@ class CartPole(env.PipelineEnv):
         # Reward Function:
         target_position = 1.0
         error = -(jnp.abs(obs[0] - target_position))
-        # Reset Conditions: If |x| >= 2.4
+        # Reset Conditions: If |x| >= 2.0
         x = jnp.abs(obs[0])
         terminal_state = jnp.array(
             [
-                jnp.where(x >= 2.4, 1.0, 0.0),
+                jnp.where(x >= 2.0, 1.0, 0.0),
             ],
         )
         done = jnp.where(terminal_state.any(), 1.0, 0.0)

brax/double_integrator/qp.py
@@ -43,13 +43,13 @@ def equality_constraints(
 
     # 1. Initial Condition Constraints:
     initial_condition = jnp.asarray([
-        x[0] - initial_conditions[0],
-        dx[0] - initial_conditions[1],
+        x[0] - initial_conditions[..., 0],
+        dx[0] - initial_conditions[..., 1],
     ], dtype=float)
 
     # 2. Collocation Constraints:
     # Model Parameters: (Match Brax model future make these learnable)
-    friction = 0.1
+    friction = 0.05
     mass = 1.0
 
     ddx = (ux - friction * dx) / mass
@@ -238,7 +238,11 @@ def qp_layer(
         params_ineq=(lb, ub),
     )
 
-    return sol, state
+    pos = sol.primal[0][:nodes]
+    vel = sol.primal[0][nodes:-nodes]
+    acc = sol.primal[0][-nodes:]
+    # return sol, state
+    return pos, vel, acc
 
 
 # Test JAXopt OSQP:
@@ -250,8 +254,8 @@ def main(argv=None) -> None:
     # Optimization Parameters: (These really matter for solve convergence)
     time_horizon = 5.0
     nodes = 51
-    num_optimizations = 1024
-    visualize_batch = 4
+    num_optimizations = 128
+    visualize_batch = 5
 
     # Dummy Inputs:
     initial_condition = []
@@ -289,18 +293,18 @@ def main(argv=None) -> None:
     vqp_layer = jax.vmap(
         vqp,
         in_axes=(0, 0),
-        out_axes=(0, 0),
+        out_axes=(0, 0, 0),
     )
 
     # Warmup:
-    _, _ = vqp_layer(
+    _, _, _ = vqp_layer(
         initial_condition,
         target_position,
     )
 
     # Solve QP:
     start_time = time.time()
-    sol, state = vqp_layer(
+    pos, vel, acc = vqp_layer(
         initial_condition,
         target_position,
     )
@@ -308,7 +312,7 @@ def main(argv=None) -> None:
     print(f'Elapsed Time: {elapsed_time:.3f} seconds')
 
     # Print Status:
-    print(f'Optimization Solved: {(state.status).any()}')
+    # print(f'Optimization Solved: {(state.status).any()}')
 
     # Plot Solution:
     fig, axes = plt.subplots(nrows=visualize_batch, ncols=2)
@@ -321,11 +325,11 @@ def main(argv=None) -> None:
     iteration = 0
     for ax in axes:
         x_plt, = ax[0].plot(
-            time_vector, sol.primal[0][iteration][:nodes],
+            time_vector, pos[iteration],
             color='royalblue', linewidth=0.75,
         )
         u_plt, = ax[1].plot(
-            time_vector, sol.primal[0][iteration][-nodes:],
+            time_vector, acc[iteration],
             color='orange', linewidth=0.75,
         )
         ax[0].hlines(