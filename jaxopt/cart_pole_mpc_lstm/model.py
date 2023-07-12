1d5c8825a201a234e0c12545891cabeb4ba0fa58
jeh15 <jeh15@fsu.edu>
2023-07-12 03:31:24 -0400

    TODO: Finish scan of MPC Cell and add LSTM Layers

--- jaxopt/cart_pole_mpc_lstm/model.py
+++ jaxopt/cart_pole_mpc_lstm/model.py
@@ -1,3 +1,5 @@
+import functools
+
 import jax
 import jax.numpy as jnp
 from flax import linen as nn
@@ -9,60 +11,148 @@ jax.config.update("jax_enable_x64", True)
 dtype = jnp.float64
 
 
+class _MPCCell(nn.Module):
+    hidden_size: int
+    env: brax.positional.base.State
+
+    def setup(self) -> None:
+        self.dense_1 = nn.Dense(
+            features=self.hidden_size,
+            name='dense_1',
+            dtype=dtype,
+        )
+        self.dense_2 = nn.Dense(
+            features=2,
+            name='dense_2',
+            dtype=dtype,
+        )
+        step_pipeline = lambda x, i: pipeline.step(self.env, x, i)
+        self.step_pipeline = jax.jit(step_pipeline)
+
+    def __call__(
+        self: Self,
+        carry: Tuple[State, PRNGKey],
+        x: jax.typing.Array,
+    ) -> Tuple[Tuple[State, PRNGKey], jax.typing.Array]:
+        state, key = carry
+        range_limit = 0.5
+        x = self.dense_1(x)
+        x = nn.tanh(x)
+        x = self.dense_2(x)
+        x_mu = range_limit * nn.tanh(x[0])
+        x_std = nn.sigmoid(x[1])
+        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
+        x = probability_distribution.sample(seed=key)
+        state = self.step_pipeline(state, x)
+        x = jnp.concatenate([state.q, state.qd])
+        _, key = jax.random.split(key)
+        carry = state, key
+        data = x
+        return carry, data
+
+
+class MPCCell(nn.Module):
+    hidden_size: int
+    env: brax.positional.base.State
+    nodes: int
+
+    def setup(self: Self) -> None:
+        init_pipeline = lambda x, i: pipeline.init(self.env, x, i)
+        self.init_pipeline = jax.jit(init_pipeline)
+
+    def __call__(self, x, key):
+        # Create initial state:
+        q = x[:2]
+        qd = x[2:]
+        state = self.init_pipeline(q, qd)
+
+        # Scan MPC Block:
+        scanMPC = nn.scan(
+            _MPCCell,
+            variable_broadcast='params',
+            split_rngs={'params': False},
+            in_axes=1,
+            out_axes=1,
+            length=self.nodes,
+        )
+        carry = state, key
+        carry, x = scanMPC(carry, x)
+
+        return x
+
+
+class __MPCBlock__(nn.Module):
+    """MPC Layer"""
+    hidden_size: int
+    env: brax.positional.base.State
+    nodes: int
+
+    def setup(self) -> None:
+        self.dense_1 = nn.Dense(
+            features=self.hidden_size,
+            name='dense_1',
+            dtype=dtype,
+        )
+        self.dense_2 = nn.Dense(
+            features=2,
+            name='dense_2',
+            dtype=dtype,
+        )
+        init_pipeline = lambda x, i: pipeline.init(self.env, x, i)
+        self.init_pipeline = jax.jit(init_pipeline)
+        step_pipeline = lambda x, i: pipeline.step(self.env, x, i)
+        self.step_pipeline = jax.jit(step_pipeline)
+
+    def __call__(self, carry, x):
+        # Scan Function:
+        def MPCCell(carry, x):
+            state, key = carry
+            range_limit = 0.5
+            x = self.dense_1(x)
+            x = nn.tanh(x)
+            x = self.dense_2(x)
+            x_mu = range_limit * nn.tanh(x[0])
+            x_std = nn.sigmoid(x[1])
+            probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
+            x = probability_distribution.sample(seed=key)
+            state = self.step_pipeline(state, x)
+            x = jnp.concatenate([state.q, state.qd])
+            _, key = jax.random.split(key)
+            carry = state, key
+            data = x
+            return carry, data
+
+        # Create initial state:
+        q = x[:2]
+        qd = x[2:]
+        state = self.init_pipeline(q, qd)
+
+        # Scan MPC Block:
+        scan = nn.scan(
+            MPCCell,
+            variable_broadcast='params',
+            split_rngs={'params': False},
+            in_axes=0,
+            out_axes=0,
+        )
+
+        return x
+
+
 class ActorCriticNetwork(nn.Module):
     action_space: int
     env: brax.positional.base.State
 
     def setup(self):
         features = 128
-        self.pipeline_layer_1 = nn.Dense(
+        self.mpc_layer_1 = nn.Dense(
             features=features,
-            name='pipeline_layer_1',
+            name='mpc_layer_1',
             dtype=dtype,
         )
-        self.pipeline_layer_3 = nn.Dense(
+        self.mpc_layer_2 = nn.Dense(
             features=features,
-            name='pipeline_layer_3',
-            dtype=dtype,
-        )
-        self.pipeline_layer_5 = nn.Dense(
-            features=features,
-            name='pipeline_layer_5',
-            dtype=dtype,
-        )
-        self.pipeline_layer_7 = nn.Dense(
-            features=features,
-            name='pipeline_layer_7',
-            dtype=dtype,
-        )
-        self.pipeline_layer_9 = nn.Dense(
-            features=features,
-            name='pipeline_layer_9',
-            dtype=dtype,
-        )
-        self.pipeline_layer_11 = nn.Dense(
-            features=features,
-            name='pipeline_layer_11',
-            dtype=dtype,
-        )
-        self.pipeline_layer_13 = nn.Dense(
-            features=features,
-            name='pipeline_layer_13',
-            dtype=dtype,
-        )
-        self.pipeline_layer_15 = nn.Dense(
-            features=features,
-            name='pipeline_layer_15',
-            dtype=dtype,
-        )
-        self.pipeline_layer_17 = nn.Dense(
-            features=features,
-            name='pipeline_layer_17',
-            dtype=dtype,
-        )
-        self.pipeline_layer_19 = nn.Dense(
-            features=features,
-            name='pipeline_layer_19',
+            name='mpc_layer_2',
             dtype=dtype,
         )
         self.dense_1 = nn.Dense(
@@ -95,56 +185,6 @@ class ActorCriticNetwork(nn.Module):
             name='dense_6',
             dtype=dtype,
         )
-        self.pipeline_layer_2 = nn.Dense(
-            features=2,
-            name='pipeline_layer_2',
-            dtype=dtype,
-        )
-        self.pipeline_layer_4 = nn.Dense(
-            features=2,
-            name='pipeline_layer_4',
-            dtype=dtype,
-        )
-        self.pipeline_layer_6 = nn.Dense(
-            features=2,
-            name='pipeline_layer_6',
-            dtype=dtype,
-        )
-        self.pipeline_layer_8 = nn.Dense(
-            features=2,
-            name='pipeline_layer_8',
-            dtype=dtype,
-        )
-        self.pipeline_layer_10 = nn.Dense(
-            features=2,
-            name='pipeline_layer_10',
-            dtype=dtype,
-        )
-        self.pipeline_layer_12 = nn.Dense(
-            features=2,
-            name='pipeline_layer_12',
-            dtype=dtype,
-        )
-        self.pipeline_layer_14 = nn.Dense(
-            features=2,
-            name='pipeline_layer_14',
-            dtype=dtype,
-        )
-        self.pipeline_layer_16 = nn.Dense(
-            features=2,
-            name='pipeline_layer_16',
-            dtype=dtype,
-        )
-        self.pipeline_layer_18 = nn.Dense(
-            features=2,
-            name='pipeline_layer_18',
-            dtype=dtype,
-        )
-        self.pipeline_layer_20 = nn.Dense(
-            features=2,
-            name='pipeline_layer_20',
-            dtype=dtype,
-        )
         self.mean_layer = nn.Dense(
             features=self.action_space,
             name='mean_layer',
@@ -166,131 +206,40 @@ class ActorCriticNetwork(nn.Module):
         self.step_pipeline = jax.jit(step_pipeline)
 
     def model(self, x, key):
-        # Limit Output Range:
-        range_limit = 0.5
+        def MPCCell(
+            carry: Tuple[jax.typing.Array, State, PRNGKey],
+            _: None,
+        ) -> Tuple[Tuple[jnp.ndarray, State, PRNGKey], jnp.ndarray]:
+            x, state, key = carry
+            x = self.mpc_layer_1(x)
+            x = nn.tanh(x)
+            x = self.mpc_layer_2(x)
+            x_mu = nn.tanh(x[0])
+            x_std = nn.sigmoid(x[1])
+            probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
+            x = probability_distribution.sample(seed=key)
+            state = self.step_pipeline(state, x)
+            x = jnp.concatenate([state.q, state.qd])
+            _, key = jax.random.split(key)
+            carry = x, state, key
+            data = x
+            return carry, data
 
         # Create Initial Pipeline State:
         q = x[:2]
         qd = x[2:]
         state = self.init_pipeline(q, qd)
 
-        # Pipeline Layer 1:
-        x = self.pipeline_layer_1(x)
-        x = nn.tanh(x)
-        x = self.pipeline_layer_2(x)
-        x_mu = range_limit * nn.tanh(x[0])
-        x_std = nn.sigmoid(x[1])
-        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
-        x = probability_distribution.sample(seed=key)
-        state = self.step_pipeline(state, x)
-        state_1 = jnp.concatenate([state.q, state.qd])
-
-        # Pipeline Layer 2:
-        x = self.pipeline_layer_3(state_1)
-        x = nn.tanh(x)
-        x = self.pipeline_layer_4(x)
-        x_mu = range_limit * nn.tanh(x[0])
-        x_std = nn.sigmoid(x[1])
-        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
-        x = probability_distribution.sample(seed=key)
-        state = self.step_pipeline(state, x)
-        state_2 = jnp.concatenate([state.q, state.qd])
-
-        # Pipeline Layer 3:
-        x = self.pipeline_layer_5(state_2)
-        x = nn.tanh(x)
-        x = self.pipeline_layer_6(x)
-        x_mu = range_limit * nn.tanh(x[0])
-        x_std = nn.sigmoid(x[1])
-        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
-        x = probability_distribution.sample(seed=key)
-        state = self.step_pipeline(state, x)
-        state_3 = jnp.concatenate([state.q, state.qd])
-
-        # Pipeline Layer 4:
-        x = self.pipeline_layer_7(state_3)
-        x = nn.tanh(x)
-        x = self.pipeline_layer_8(x)
-        x_mu = range_limit * nn.tanh(x[0])
-        x_std = nn.sigmoid(x[1])
-        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
-        x = probability_distribution.sample(seed=key)
-        state = self.step_pipeline(state, x)
-        state_4 = jnp.concatenate([state.q, state.qd])
-
-        # Pipeline Layer 5:
-        x = self.pipeline_layer_9(state_4)
-        x = nn.tanh(x)
-        x = self.pipeline_layer_10(x)
-        x_mu = range_limit * nn.tanh(x[0])
-        x_std = nn.sigmoid(x[1])
-        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
-        x = probability_distribution.sample(seed=key)
-        state = self.step_pipeline(state, x)
-        state_5 = jnp.concatenate([state.q, state.qd])
-
-        # Pipeline Layer 6:
-        x = self.pipeline_layer_11(state_5)
-        x = nn.tanh(x)
-        x = self.pipeline_layer_12(x)
-        x_mu = range_limit * nn.tanh(x[0])
-        x_std = nn.sigmoid(x[1])
-        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
-        x = probability_distribution.sample(seed=key)
-        state = self.step_pipeline(state, x)
-        state_6 = jnp.concatenate([state.q, state.qd])
-
-        # Pipeline Layer 7:
-        x = self.pipeline_layer_13(state_6)
-        x = nn.tanh(x)
-        x = self.pipeline_layer_14(x)
-        x_mu = range_limit * nn.tanh(x[0])
-        x_std = nn.sigmoid(x[1])
-        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
-        x = probability_distribution.sample(seed=key)
-        state = self.step_pipeline(state, x)
-        state_7 = jnp.concatenate([state.q, state.qd])
-
-        # Pipeline Layer 8:
-        x = self.pipeline_layer_15(state_7)
-        x = nn.tanh(x)
-        x = self.pipeline_layer_16(x)
-        x_mu = range_limit * nn.tanh(x[0])
-        x_std = nn.sigmoid(x[1])
-        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
-        x = probability_distribution.sample(seed=key)
-        state = self.step_pipeline(state, x)
-        state_8 = jnp.concatenate([state.q, state.qd])
-
-        # Pipeline Layer 9:
-        x = self.pipeline_layer_17(state_8)
-        x = nn.tanh(x)
-        x = self.pipeline_layer_18(x)
-        x_mu = range_limit * nn.tanh(x[0])
-        x_std = nn.sigmoid(x[1])
-        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
-        x = probability_distribution.sample(seed=key)
-        state = self.step_pipeline(state, x)
-        state_9 = jnp.concatenate([state.q, state.qd])
-
-        # Pipeline Layer 10:
-        x = self.pipeline_layer_19(state_9)
-        x = nn.tanh(x)
-        x = self.pipeline_layer_20(x)
-        x_mu = range_limit * nn.tanh(x[0])
-        x_std = nn.sigmoid(x[1])
-        probability_distribution = distrax.Normal(loc=x_mu, scale=x_std)
-        x = probability_distribution.sample(seed=key)
-        state = self.step_pipeline(state, x)
-        state_10 = jnp.concatenate([state.q, state.qd])
-
-        # Trajectory:
-        state_trajectory = jnp.concatenate(
-            [
-                state_1, state_2, state_3, state_4, state_5,
-                state_6, state_7, state_8, state_9, state_10,
-            ],
+        # Scan MPC Block:
+        carry, data = jax.lax.scan(
+            f=MPCCell,
+            init=(x, state, key),
+            xs=None,
+            length=self.nodes,
         )
+        x = data
+        # Trajectory:
+        state_trajectory = x
 
         # Mean Layer:
         i = self.dense_1(state_trajectory)
@@ -310,7 +259,7 @@ class ActorCriticNetwork(nn.Module):
 
         # Output Layer:
         mean = self.mean_layer(i)
-        mean = range_limit * nn.tanh(mean)
+        mean = nn.tanh(mean)
         std = self.std_layer(j)
         std = nn.sigmoid(std)
         values = self.value_layer(k)
