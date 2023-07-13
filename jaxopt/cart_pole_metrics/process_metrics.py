9a005e04effe9278134500d72142a6e0a3496846
jeh15 <jeh15@fsu.edu>
2023-07-13 03:27:34 -0400

    TODO: Share Policy Layer. Debug UR5e environment.

--- jaxopt/cart_pole_metrics/process_metrics.py
+++ jaxopt/cart_pole_metrics/process_metrics.py
@@ -9,14 +9,18 @@ import matplotlib.pyplot as plt
 def main(argv=None):
     # Load Metrics:
     filepath = os.path.dirname(os.path.abspath(__file__))
-    metrics_ppo_path = os.path.join(filepath, "metrics_shared.pkl")
+    metrics_ppo_path = os.path.join(filepath, "metrics_ppo_v2.pkl")
     metrics_mpc_path = os.path.join(filepath, "metrics_mpc_v2.pkl")
+    metrics_shared_path = os.path.join(filepath, "metrics_shared.pkl")
 
     with open(metrics_ppo_path, "rb") as f:
         metrics_ppo = pickle.load(f)
 
     with open(metrics_mpc_path, "rb") as f:
         metrics_mpc = pickle.load(f)
+
+    with open(metrics_shared_path, "rb") as f:
+        metrics_shared = pickle.load(f)
 
     reward_history_ppo = np.asarray(metrics_ppo['reward_history'])
     reward_ppo = np.mean(
@@ -38,19 +42,31 @@ def main(argv=None):
     time_vector_mpc = time_vector_mpc - time_vector_mpc[0]
     # time_vector_mpc = time_vector_mpc / 60.0
 
+    reward_history_shared = np.asarray(metrics_shared['reward_history'])
+    reward_shared = np.mean(
+        np.sum(reward_history_shared, axis=-1),
+        axis=-1,
+    )
+    loss_history_shared = np.asarray(metrics_shared['loss_history'])
+    time_vector_shared = np.asarray(metrics_shared['time_history'])
+    time_vector_shared = time_vector_shared - time_vector_shared[0]
+    # time_vector_shared = time_vector_shared / 60.0
+
     # Plot Metrics:
     fig, ax = plt.subplots(2)
     fig.tight_layout(pad=4.0)
 
-    ax[0].plot(reward_ppo, label='MPC Shared', color='darkorange', linewidth=2)
-    ax[0].plot(reward_mpc, label='MPC Layer', color='cornflowerblue', linewidth=2)
+    ax[0].plot(reward_ppo, label='Pure PPO', color='darkorange', linewidth=1.5)
+    ax[0].plot(reward_mpc, label='MPC Layer', color='cornflowerblue', linewidth=1.5)
+    ax[0].plot(reward_shared, label='MPC Layer Shared', color='limegreen', linewidth=1.5)
     ax[0].set_title("Total Reward per Episode (avg. across batch)", fontsize=12)
     ax[0].set_xlabel("Epoch", fontsize=10)
     ax[0].set_ylabel("Total Episode Reward", fontsize=10)
     ax[0].legend(fontsize=8)
 
-    ax[1].plot(loss_history_ppo, label='MPC Shared', color='darkorange', linewidth=2)
-    ax[1].plot(loss_history_mpc, label='MPC Layer', color='cornflowerblue', linewidth=2)
+    ax[1].plot(loss_history_ppo, label='MPC Shared', color='darkorange', linewidth=1.5)
+    ax[1].plot(loss_history_mpc, label='MPC Layer', color='cornflowerblue', linewidth=1.5)
+    ax[1].plot(loss_history_shared, label='MPC Layer Shared', color='limegreen', linewidth=1.5)
     ax[1].set_title("Loss", fontsize=12)
     ax[1].set_xlabel("Epoch", fontsize=10)
     ax[1].set_ylabel("Loss", fontsize=10)
@@ -63,18 +79,20 @@ def main(argv=None):
     fig, ax = plt.subplots(2)
     fig.tight_layout(pad=4.0)
 
-    ax[0].plot(time_vector_ppo, reward_ppo, label='Pure PPO', color='darkorange', linewidth=2)
-    ax[0].plot(time_vector_mpc, reward_mpc, label='MPC Layer', color='cornflowerblue', linewidth=2)
-    ax[0].set_xscale('log')
+    ax[0].plot(time_vector_ppo, reward_ppo, label='Pure PPO', color='darkorange', linewidth=1)
+    ax[0].plot(time_vector_mpc, reward_mpc, label='MPC Layer', color='cornflowerblue', linewidth=1)
+    ax[0].plot(time_vector_shared, reward_shared, label='MPC Layer Shared', color='limegreen', linewidth=1)
+    # ax[0].set_xscale('log')
     ax[0].set_title("Total Reward per Episode (avg. across batch)", fontsize=12)
     ax[0].set_xlabel("Time (s)", fontsize=10)
     # ax[0].set_xlabel("Time (min)", fontsize=10)
     ax[0].set_ylabel("Total Episode Reward", fontsize=10)
     ax[0].legend(fontsize=8)
 
-    ax[1].plot(time_vector_ppo, loss_history_ppo, label='Pure PPO', color='darkorange', linewidth=2)
-    ax[1].plot(time_vector_mpc, loss_history_mpc, label='MPC Layer', color='cornflowerblue', linewidth=2)
-    ax[1].set_xscale('log')
+    ax[1].plot(time_vector_ppo, loss_history_ppo, label='Pure PPO', color='darkorange', linewidth=1)
+    ax[1].plot(time_vector_mpc, loss_history_mpc, label='MPC Layer', color='cornflowerblue', linewidth=1)
+    ax[1].plot(time_vector_shared, loss_history_shared, label='MPC Layer Shared', color='limegreen', linewidth=1)
+    # ax[1].set_xscale('log')
     ax[1].set_title("Loss", fontsize=12)
     ax[1].set_xlabel("Time (s)", fontsize=10)
     # ax[1].set_xlabel("Time (min)", fontsize=10)
@@ -82,7 +100,7 @@ def main(argv=None):
     ax[1].legend(fontsize=8)
 
     plt.show()
-    plt.savefig(os.path.join(filepath, "metrics_log_time.pdf"))
+    plt.savefig(os.path.join(filepath, "metrics_time.pdf"))
 
 
 if __name__ == "__main__":
